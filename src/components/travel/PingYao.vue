3c4dcf42fdeb65dbf0df345236daf1c6ca80d70f
youngfreeFJS <471011042@qq.com>
2020-11-09 17:17:45 +0800

    test

---
+++ src/components/travel/PingYao.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        pingyao
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/utils/routerUtils.js
@@ -0,0 +1,7 @@
+export const getlink1 = () => {
+  return 1
+}
+
+export const getlink2 = () => {
+  return 2
+}
